8f57cc8953692438f5af135455b9f1dd9b14c6e5
flowermouse <haushulizi@gmail.com>
2025-12-08 20:25:45 +0800

    fix

--- ms_agent/agent/llm_agent.py
+++ ms_agent/agent/llm_agent.py
@@ -450,9 +450,6 @@ class LLMAgent(Agent):
         Returns:
             List[Message]: Updated message history after this step.
         """
-        print("="*50, "SimpleAgent Step", "=="*50)
-        print(messages)
-        print("=="*50)
         messages = deepcopy(messages)
         if (not self.load_cache) or messages[-1].role != 'assistant':
             messages = await self.condense_memory(messages)
