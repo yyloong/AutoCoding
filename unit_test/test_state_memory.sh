41f6ab7221756b591236d1e6c4121bf035ca03f8
flowermouse <haushulizi@gmail.com>
2025-12-11 19:10:02 +0800

    fix bug

--- unit_test/test_state_memory.sh
+++ unit_test/test_state_memory.sh
@@ -1,4 +1,4 @@
 rm -rf memory
 rm -rf new_output
 mkdir new_output
-python -m ms_agent/cli/cli.py run --config unit_test/test_state_memory --query 'auction start' --trust_remote_code true --load_cache true
+python -m ms_agent.cli.cli run --config unit_test/test_state_memory --query 'auction start' --trust_remote_code true --load_cache true
